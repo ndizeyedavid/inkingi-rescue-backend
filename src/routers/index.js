eb0f404622db243d8284cb25cb903a41460174df
ndizeyedavid <davidndizeye101@gmail.com>
2025-04-06 14:54:26 +0200

    added role based access to some services

--- src/routers/index.js
+++ src/routers/index.js
@@ -4,13 +4,14 @@ import commentRouter from "./routes/comment.route.js";
 // import messageRouter from "./routes/message.route.js";
 import volunteerRouter from "./routes/volunteer.route.js";
 import sosRouter from "./routes/sos.route.js";
+import { verifyToken } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.use("/users", userRouter);
-router.use("/sos", sosRouter);
-router.use("/comment", commentRouter);
+router.use("/sos", verifyToken, sosRouter);
+router.use("/comment", verifyToken, commentRouter);
 // router.use("/message", messageRouter);  // coming soon
-router.use("/volunteer", volunteerRouter);
+router.use("/volunteer", verifyToken, volunteerRouter);
 
 export default router;
--- src/routers/routes/users.route.js
+++ src/routers/routes/users.route.js
@@ -1,5 +1,6 @@
 import express from "express";
 import usersController from "../../controllers/users.controller.js";
+import { verifyToken } from "../../middleware/auth.middleware.js";
 
 const userRouter = express.Router();
 
@@ -7,13 +8,13 @@ userRouter.post("/auth/signup", usersController.signup);
 userRouter.post("/auth/login", usersController.login);
 
 // normal CRUD
-userRouter.get("/view/all", usersController.getAllUsers);
-userRouter.get("/view/:id", usersController.getUser);
-userRouter.put("/update/:id", usersController.updateUser);
-userRouter.delete("/delete/:id", usersController.deleteUser);
+userRouter.get("/view/all", verifyToken, usersController.getAllUsers);
+userRouter.get("/view/:id", verifyToken, usersController.getUser);
+userRouter.put("/update/:id", verifyToken, usersController.updateUser);
+userRouter.delete("/delete/:id", verifyToken, usersController.deleteUser);
 
 // security
-userRouter.post("/auth/change-password", usersController.changePassword);
+userRouter.post("/auth/change-password", verifyToken, usersController.changePassword);
 // userRouter.post("/auth/forgot-password", usersController.forgotPassword); // coming soon
 // userRouter.post("/auth/reset-password", usersController.resetPassword); // coming soon
 
