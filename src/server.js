5767299f1774b76b31c7bc5c76c7a920d309fadc
ndizeyedavid <davidndizeye101@gmail.com>
2025-09-10 11:18:47 +0200

    Alive endpoint

--- src/server.js
+++ src/server.js
@@ -10,20 +10,22 @@ const app = express();
 
 app.use(express.json());
 app.use(
-    cors({
-        origin: "http://localhost:5173",
-        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-        allowedHeaders: ["Content-Type", "Authorization"],
-    })
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
 );
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log("Server is running on port:", port);
+  console.log("Server is running on port:", port);
 });
 
 dbConnect();
-
+app.get("/health", (req, res) => {
+  res.status(200).json({ message: "Backend is live and operatable" });
+});
 app.use("/api", router);
 app.use("/uploads", express.static("uploads"));
